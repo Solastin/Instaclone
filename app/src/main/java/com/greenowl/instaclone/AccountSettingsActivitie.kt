b3b5105981f87075fb779b2301f9fda3fe064f5d
Solastin <raphael.greenowl@gmail.com>
2020-07-30 00:42:59 -0300

    30/07 - 00:42

---
+++ app/src/main/java/com/greenowl/instaclone/AccountSettingsActivitie.kt
@@ -0,0 +1,11 @@
+package com.greenowl.instaclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountSettingsActivitie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_settings_activitie)
+    }
+}
